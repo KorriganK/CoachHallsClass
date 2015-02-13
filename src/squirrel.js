7297ba88d8fa22ed0df4fe465c166b8912cec487
KorriganK <kory.korthauer@gmail.com>
2015-02-13 08:48:32 -0600

    Update squirrel.js

--- src/squirrel.js
+++ src/squirrel.js
@@ -1,4 +1,6 @@
-var ctx = document.getElementById("c").getContext("2d"); 
+var ctx = document.getElementById("c").getContext("2d");
+var back = new Image();
+back.src="https://cloud.githubusercontent.com/assets/7268773/6169905/2634dea6-b296-11e4-9db6-b0b7733d9bc6.png";
 function onKeyDown(e) {
 	if (e.keyCode == 68 || e.keyCode == 100) rightDown = true;
 	else if (e.keyCode == 65 || e.keyCode == 97) leftDown = true;
@@ -12,10 +14,13 @@ function onKeyUp(e) {
 	else if (e.keyCode == 115 || e.keyCode == 83) downDown = false;
 }
 function clear() {
-  ctx.clearRect(0,0,600,600);
+ 	ctx.clearRect(0,0,600,600);
+}
+function background() {
+	ctx.drawImage(back, 0, 0);
 }
 function draw() {
-  clear();
+  background();
 }
 var i = setInterval(draw, 20)
 document.addEventListener('keydown', onKeyDown);
